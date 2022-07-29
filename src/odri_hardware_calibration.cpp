e211c29eea2506eeaf5cbdfe28777f4d2f8117c0
Maxime Fansi <mufansitch@laas.fr>
2022-07-29 18:09:53 +0200

    Remove path to robot yaml file through macro. Uses argument instead.

--- src/odri_hardware_calibration.cpp
+++ src/odri_hardware_calibration.cpp
@@ -3,9 +3,9 @@
 
 /*Modifs :
 
-   Vector12d --> Vector6d
-   Boucles for pour les couples : i<12 --> i<6
-   CONFIG_ROBOT_YAML
+  Vector12d --> Vector6d
+  Boucles for pour les couples : i<12 --> i<6
+  CONFIG_ROBOT_YAML
 
 */
 
@@ -20,81 +20,97 @@ using namespace odri_control_interface;
 
 #include <iostream>
 #include <stdexcept>
+#include <fstream>
 
 typedef Eigen::Matrix<double, 6, 1> Vector6d;
 
-int main()
+int main(int argc, char *argv[])
 {
-    nice(-20);  // Give the process a high priority.
+  nice(-20);  // Give the process a high priority.
 
-    // Define the robot from a yaml file.
-    auto robot = RobotFromYamlFile(CONFIG_ROBOT_YAML);
-
-    // Start the robot.
-    robot->Start();
-
-    // Define controller to calibrate the joints from yaml file.
-    auto calib_ctrl = JointCalibratorFromYamlFile(
-        CONFIG_ROBOT_YAML, robot->joints);
-    Eigen::Matrix<double,6,1> zero6=Eigen::Matrix<double,6,1>::Zero();
-    calib_ctrl->UpdatePositionOffsets(zero6);
-    // Initialize simple pd controller.
-    Vector6d torques;
-    int c = 0;
-    std::chrono::time_point<std::chrono::system_clock> last =
-        std::chrono::system_clock::now();
-    bool is_calibrated = false;
-    while (!robot->IsTimeout())
+  if (argc!=2)
     {
-        if (((std::chrono::duration<double>)(std::chrono::system_clock::now() -
-                                             last))
-                .count() > 0.001)
-        {
-            last = std::chrono::system_clock::now();  // last+dt would be better
-            robot->ParseSensorData();
+      std::cerr << argv[0] << " takes only one argument the robot config yaml." << std::endl;
+      std::cerr << argv[0] << " This argument is mandatory." << std::endl;
+      exit(-1);
+    }
 
-            if (robot->IsReady())
+  std::ifstream aif(argv[1]);
+
+  if (!aif.is_open())
+    {
+      std::cerr << argv[1] << " does not exist !" << std::endl;
+      exit(-1);
+    }
+  aif.close();
+      
+  // Define the robot from a yaml file.
+  auto robot = RobotFromYamlFile(argv[1]);
+
+  // Start the robot.
+  robot->Start();
+
+  // Define controller to calibrate the joints from yaml file.
+  auto calib_ctrl = JointCalibratorFromYamlFile(argv[1], robot->joints);
+  Eigen::Matrix<double,6,1> zero6=Eigen::Matrix<double,6,1>::Zero();
+  calib_ctrl->UpdatePositionOffsets(zero6);
+  // Initialize simple pd controller.
+  Vector6d torques;
+  int c = 0;
+  std::chrono::time_point<std::chrono::system_clock> last =
+    std::chrono::system_clock::now();
+  bool is_calibrated = false;
+  while (!robot->IsTimeout())
+    {
+      if (((std::chrono::duration<double>)(std::chrono::system_clock::now() -
+                                           last))
+          .count() > 0.001)
+        {
+          last = std::chrono::system_clock::now();  // last+dt would be better
+          robot->ParseSensorData();
+
+          if (robot->IsReady())
             {
-                if (!is_calibrated)
+              if (!is_calibrated)
                 {
-                    is_calibrated = calib_ctrl->Run();
-                    if (is_calibrated)
+                  is_calibrated = calib_ctrl->Run();
+                  if (is_calibrated)
                     {
-                        std::cout << "Calibration is done." << std::endl;
+                      std::cout << "Calibration is done." << std::endl;
                     }
                 }
-                else
+              else
                 {
-                    // Run the main controller.
-                    auto pos = robot->joints->GetPositions();
-                    auto vel = robot->joints->GetVelocities();
-                    // Reverse the positions;
-                    for (int i = 0; i < 6; i++)
+                  // Run the main controller.
+                  auto pos = robot->joints->GetPositions();
+                  auto vel = robot->joints->GetVelocities();
+                  // Reverse the positions;
+                  for (int i = 0; i < 6; i++)
                     {
-                        torques[i] = 0.0; //-kp * pos[i] - kd * vel[i]
+                      torques[i] = 0.0; //-kp * pos[i] - kd * vel[i]
                     }
-                    robot->joints->SetTorques(torques);
+                  robot->joints->SetTorques(torques);
                 }
             }
 
-            // Checks if the robot is in error state (that is, if any component
-            // returns an error). If there is an error, the commands to send
-            // are changed to send the safety control.
-            robot->SendCommand();
+          // Checks if the robot is in error state (that is, if any component
+          // returns an error). If there is an error, the commands to send
+          // are changed to send the safety control.
+          robot->SendCommand();
 
-            c++;
-            if (c % 1000 == 0)
+          c++;
+          if (c % 1000 == 0)
             {
-                std::cout << "Joints: ";
-                robot->joints->PrintVector(-robot->joints->GetPositions());
-                std::cout << std::endl;
+              std::cout << "Joints: ";
+              robot->joints->PrintVector(-robot->joints->GetPositions());
+              std::cout << std::endl;
             }
         }
-        else
+      else
         {
-            std::this_thread::yield();
+          std::this_thread::yield();
         }
     }
-    std::cout << "Normal program shutdown." << std::endl;
-    return 0;
+  std::cout << "Normal program shutdown." << std::endl;
+  return 0;
 }
